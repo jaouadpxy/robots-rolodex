b4ec7ed50dd7d091db416f10c11602e78a0767d8
Jaouad <jaouad.pxy@gmail.com>
2019-09-30 12:27:55 +1000

    Updates

---
+++ precache-manifest.0ac4e34823081ecc9117338eb8282463.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972d8e6c9212bc8965224c0dc418478c",
+    "url": "/jaouadpxy/robots-rolodex/index.html"
+  },
+  {
+    "revision": "904cd7fff50a1049432d",
+    "url": "/jaouadpxy/robots-rolodex/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1daa0a83875d80212fb9",
+    "url": "/jaouadpxy/robots-rolodex/static/css/main.d142ade0.chunk.css"
+  },
+  {
+    "revision": "904cd7fff50a1049432d",
+    "url": "/jaouadpxy/robots-rolodex/static/js/2.3b911aba.chunk.js"
+  },
+  {
+    "revision": "1daa0a83875d80212fb9",
+    "url": "/jaouadpxy/robots-rolodex/static/js/main.6c9269f0.chunk.js"
+  },
+  {
+    "revision": "53c918f442299302c9e4",
+    "url": "/jaouadpxy/robots-rolodex/static/js/runtime-main.cfe7bc6b.js"
+  }
+]);
